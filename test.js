3b49c47712c9e1acebc3b015ee2d779b71b213e7
henry <henry@incuna.com>
2012-08-18 18:35:03 +0100

    Finalised test page styling. @TODO refactor static css and html text changes into jquery text changes. Much nicer.

--- test.js
+++ test.js
@@ -1,22 +1,17 @@
-$(function(){
-    $('.move').hover(
-        function(){
-            $(this).css('background-color', '#000').css('color', '#fff');
-        },
-        function(){
-            $(this).css('background-color', '#fff').css('color', '#000');
-        }
-    );
-});
 $(function() {
+    // Add jQuery-ui sortable.
     $('#mover').sortable({
+        // Before removing the placeholder/helper, capture the offset and apply
+        // it to the element.
         beforeStop: function(event, ui) {
-            console.log(ui);
             var item = $(ui.item[0]);
-            var offset = ui.offset;
+            var offset = ui.offset; //This contains positional values as props
+            // Adds absolute positioning
             offset.position = "absolute";
-            item.css(offset);
+            // Apply the offset style and add our own status class.
+            item.css(offset).addClass('hasmoved');
         }
     });
-    $( "#sortable" ).disableSelection();
+    // This disables selection. Whodathunkit?
+    $( "#mover" ).disableSelection();
 });
